e13f1f9495ba16662d7e88f6b8a4b85da4a94787
Diogo Pinheiro Murta Freire <diogomurta@gmail.com>
2020-10-27 00:19:16 -0300

    Adicionando encode64

--- encode_join.py
+++ encode_join.py
@@ -1,20 +1,32 @@
 import base64
+import os
 
-message = open("~/.kube/config")
-message_bytes = message.encode('ascii')
+
+message = open(os.path.expanduser('~/.kube/config'), "r").read()
+
+print("Kubeconfig => " + "\n" + "#-----------#" +"\n" + message + "\n" + "#-----------#" )
+
+message_bytes = message.encode('UTF-8')
 base64_bytes = base64.b64encode(message_bytes)
-base64_message = base64_bytes.decode('ascii')
+base64_message = base64_bytes.decode('UTF-8')
 
 print(base64_message)
 
-base64_message = file.write('fileencode.txt')
+file = open("fileencode.txt", "w")
+str_base64_message = repr(base64_message)
+file.write(str_base64_message)
 file.close()
 
-print(base64_message)
+
 
 
 f = open("fileencode.txt", "r")
 config = ""
 for line in f:
     config += line.split("\n")[0]
+# print(config)
+file = open("fileencode-split.txt", "w")
+str_split_base64_message = repr(config)
+file.write(str_split_base64_message)
+file.close()
 print(config)
